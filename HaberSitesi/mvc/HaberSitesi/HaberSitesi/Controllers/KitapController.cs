a715e24d95f6d84774fe19840cd2fc6b364b2dae
Recep Çelimli <98876147+celimlirecep@users.noreply.github.com>
2022-04-12 14:59:58 +0300

    HaberSitesi_Clon

---
+++ HaberSitesi/mvc/HaberSitesi/HaberSitesi/Controllers/KitapController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HaberSitesi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaberSitesi.Controllers
+{
+    public class KitapController : Controller
+    {
+        public IActionResult Index()
+        {
+            var kitaplar = KitapData.GetAllKitaplar();
+            return View(kitaplar);
+        }
+
+        public IActionResult KitapDetay()
+        {
+            int ID = Convert.ToInt32(ControllerContext.RouteData.Values["ID"]);
+            var kitaplar = KitapData.GetAllKitaplar();
+            Kitaplar secilenKitap = null;
+            foreach(var kitap in kitaplar)
+            {
+                if (kitap.ID==ID)
+                {
+                    secilenKitap = kitap;
+                    break;
+                }
+            }
+
+            ViewBag.kitap = secilenKitap;
+            return View();
+        }
+
+
+
+
+
+    }
+}
---
+++ HaberSitesi/mvc/HaberSitesi/HaberSitesi/Models/KitapData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaberSitesi.Models
+{
+    public static class KitapData
+    {
+        public static List<Kitaplar> GetAllKitaplar()
+        {
+            var kitaplar = new List<Kitaplar>
+            {
+                new Kitaplar{ID= 15,KitapAdi="Araba Sevdası1"},
+                new Kitaplar{ID= 16,KitapAdi="Araba Sevdası2"},
+                new Kitaplar{ID= 17,KitapAdi="Araba Sevdası3"},
+                new Kitaplar{ID= 18,KitapAdi="Araba Sevdası4"},
+                new Kitaplar{ID= 19,KitapAdi="Araba Sevdası5"},
+                new Kitaplar{ID= 20,KitapAdi="Araba Sevdası6"},
+
+
+            };
+
+            return kitaplar;
+        }
+
+
+    }
+}
